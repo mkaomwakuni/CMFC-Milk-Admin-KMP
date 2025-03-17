6105577b1ccd1011bd587321180fcf874eb1fd7d
mkaomwakuni <mkaoibrahim@gmail.com>
2025-03-17 19:45:00 +0300

    feat: create stock summary repository implementation

---
+++ composeApp/src/commonMain/kotlin/cnc/coop/milkcreamies/data/repository/StockSummaryRepositoryImpl.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2025  MkaoCodes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cnc.coop.milkcreamies.data.repository
+
+import cnc.coop.milkcreamies.domain.repository.StockSummaryRepository
+import cnc.coop.milkcreamies.models.StockSummary
+import cnc.coop.milkcreamies.data.remote.MilkClient
+import kotlinx.datetime.LocalDate
+
+class StockSummaryRepositoryImpl(
+    private val milkClient: MilkClient
+) : StockSummaryRepository {
+    override suspend fun getStockSummary(currentDate: LocalDate): Result<StockSummary> {
+        return milkClient.getStockSummary(currentDate)
+    }
+}
