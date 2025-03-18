92e465df393e87fb35b639bbbdcc0f4b54d858b5
mkaomwakuni <mkaoibrahim@gmail.com>
2025-03-18 09:30:00 +0300

    feat: implement earnings summary repository

---
+++ composeApp/src/commonMain/kotlin/cnc/coop/milkcreamies/data/repository/EarningsSummaryRepositoryImpl.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2025  MkaoCodes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cnc.coop.milkcreamies.data.repository
+
+import cnc.coop.milkcreamies.domain.repository.EarningsSummaryRepository
+import cnc.coop.milkcreamies.models.EarningsSummary
+import cnc.coop.milkcreamies.data.remote.MilkClient
+import kotlinx.datetime.LocalDate
+
+class EarningsSummaryRepositoryImpl(
+    private val milkClient: MilkClient
+) : EarningsSummaryRepository {
+    override suspend fun getEarningsSummary(currentDate: LocalDate): Result<EarningsSummary> {
+        return milkClient.getEarningsSummary(currentDate)
+    }
+}
